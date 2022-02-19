821f2cc34e65bae82fcbcdb720293a3c934441a8
Arfan0306 <99815861+Arfan0306@users.noreply.github.com>
2022-02-19 16:07:56 +0800

    Update dey1.java

--- dey1.java
+++ dey1.java
@@ -3,7 +3,7 @@ package Tugas makul algoritma struktur dan data;
 public class dey1 {
 
   public static void main(String[] args) {
-  System.out.println("Welcom to java)"
+  System.out.println("Welcom to java");
    
   }
 }
