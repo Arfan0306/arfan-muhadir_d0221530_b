e5f034e8cbb327e4d204e559a28810a8175d2d70
Arfan0306 <99815861+Arfan0306@users.noreply.github.com>
2022-02-20 19:50:42 +0800

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,16 @@
+package tugas;
+import java.util.*;
+
+public class day2 {
+
+public static void main(String[] args) {
+  Scanner scan = new Scanner(System.in);
+  int a = scan.nextInt();
+  int x = scan.nextInt();
+  int y = scan.nextInt();
+
+  System.out.println(a);
+  System.out.println(x);
+  System.out.println(y);
+  }
+}
